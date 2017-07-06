ed8fd99bc98925434ee45b1c6ab71081f6509773
aodbee <ozzaut@gmail.com>
2017-07-06 14:36:59 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 /**
- * Copyright (c) 2015-present, Callstack Sp z o.o.
- * All rights reserved.
- *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
