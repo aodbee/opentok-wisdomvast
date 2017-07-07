e1258abe33c06df4c41f2dea5e2f78a2eb322082
aodbee <ozzaut@gmail.com>
2017-07-07 08:16:28 +0700

    fix bug

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 
 import { requireNativeComponent } from 'react-native';
 
-export const PublisherView = require('./PublisherView').default;
-export const SubscriberView = require('./SubscriberView').default;
+export const PView = require('./PublisherView').default;
+export const SubView = require('./SubscriberView').default;
 export const Session = require('./Session');
